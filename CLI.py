04baee7f2f882b7d47c5bf4d7b4cc53de8f9973d
Samory Pereira Santos <samory.santos@gmail.com>
2019-06-26 00:02:44 -0300

    Added localisation Added option to update money

--- CLI.py
+++ CLI.py
@@ -3,6 +3,19 @@ import argparse as ap
 import datetime as dt
 import inflationtools.main as main
 from argparse import RawTextHelpFormatter # Allows to use newline in help text
+import locale
+import gettext # Unable to get pot for this file... find the reason.
+
+pt = gettext.translation('CLI', localedir='locales', languages=['pt_BR'])
+
+sys_locale = locale.getdefaultlocale()
+if 'BR' in sys_locale[0]:
+    pt.install()
+    _ = pt.gettext
+
+locale.setlocale(locale.LC_NUMERIC,
+                 sys_locale[0][0:2])# Sets locales to system default for numbers
+locale.setlocale(locale.LC_MONETARY, 'pt') # Sets locales to Brazil, for money
 
 # Prepares indexes list.
 indexes = {}
@@ -10,6 +23,7 @@ indexes.update(main.bcb_urls)
 indexes.update(main.quandl_urls)
 indexes = list(indexes.keys())
 indexes.sort()
+indexes = '\n'.join(indexes)
 
 # Date parser
 def parse_dates(date_string):
@@ -22,22 +36,29 @@ def parse_dates(date_string):
 def CLI():
     """
     Implements the argument parser to inflationtools.
-    TODO
-    localisation to portuguese (BR)
     :return:
     """
     parser = ap.ArgumentParser(formatter_class=RawTextHelpFormatter)
-    parser.add_argument('index',
-                        help='The inflation index that you want to look. Available: \n{}'.format(
-                            '\n'.join(indexes)
-                        ))
-    parser.add_argument('start_date', help="Starting date, using '01-2001' format.")
-    parser.add_argument('end_date', help="Ending date, using '01-2001' format.")
+    parser.add_argument('index', metavar=_('index'),
+                        help=_('The inflation index that you want to look. Available: \n') + indexes)
+    parser.add_argument('start_date', metavar=_('start_date'),
+                        help=_("Starting date, using '01-2001' format."))
+    parser.add_argument('end_date', metavar=_('end_date'),
+                        help=_("Ending date, using '01-2001' format."))
+    parser.add_argument('-a', '--amount', metavar=_('amount'),
+                        help=_('Amount you want to update.'))
     arguments = parser.parse_args()
     arguments.start_date, arguments.end_date = parse_dates(arguments.start_date), parse_dates(arguments.end_date)
-    return print(main.get_cumulative_inflation(arguments.index,
-                                               arguments.start_date,
-                                               arguments.end_date))
+    inflation = main.get_cumulative_inflation(arguments.index, arguments.start_date, arguments.end_date)
+    if arguments.amount:
+        money = arguments.amount
+        if money[0:2] == 'R$':
+            money = money[2:]
+        money = locale.atof(money)
+        money *= inflation
+        print(locale.currency(money)) # Prints in BRL
+    else:
+        print(locale.str(inflation))
 
 if __name__ == '__main__':
     CLI()
